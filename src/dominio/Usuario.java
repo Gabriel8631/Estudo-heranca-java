16c15aed50e449f25df0968c604ddd1747b29948
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2026-04-09 18:43:39 -0300

    Create Usuario class with name and email properties

---
+++ src/dominio/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario extends EntidadeBase<Long> {
+    private String nome;
+    private String email;
+
+    public Usuario(Long id, String nome, String email) {
+        this.setId(id);
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

f544205f525ad0c1f07c400f79c61c5590db7968
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2026-04-09 18:23:23 -0300

    Create RepositorioGenerico interface for generic repo

---
+++ RepositorioGenerico.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.Optional;
+
+public interface RepositorioGenerico<T extends EntidadeBase<ID>, ID> {
+    void salvar(T entidade);
+    Optional<T> buscarPorId(ID id);
+    List<T> buscarTodos();
+    void deletar(ID id);
+}
